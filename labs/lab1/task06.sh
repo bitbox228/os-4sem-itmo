fa9d94ca5b31f66872e09fd57a1908a5883350fa
nikita <368050@edu.itmo.ru>
2024-02-20 18:09:31 +0300

    fix lab01 task06 task07

--- labs/lab1/task06.sh
+++ labs/lab1/task06.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-warnings=""
-infos=""
-
-while IFS= read -r line
-  do
-    if [[ $line == *"(**)"* ]]; then
-      warnings+="\n${line/(**)/Warning}"
-    else
-      infos+="\n${line/(II)/Information}"
-    fi
-done < /var/log/anaconda/X.log
-
-echo -e "$warnings" > full.log
-echo -e "$infos" >> full.log
+awk '{if ($3 == "(II)") { $3 = "Information"; print $0} else if ($3 == "(WW)") { $3 = "Warning"; print $0; } }' /var/log/anaconda/X.log > full.log
--- labs/lab1/task07.sh
+++ labs/lab1/task07.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-egrep -r -o '((\w+)(\.|\-|\_)*)+\@\w+(\.\w+)+' /etc | awk -F: '{$2!=" ";printf("%s, ", $2)}' > email.lst
+egrep -r -o '((\w+)(\.|\-|\_)*)+\@\w+(\.\w+)+' /etc | awk -F: '{ if ($2 != "") printf("%s, ", $2); }' > email.lst
