04a8dbaa707a93274a9fc0f8047ad10d9a37e9d9
nikita <368050@edu.itmo.ru>
2024-02-22 11:37:11 +0300

    upd task10

--- labs/lab1/task10.sh
+++ labs/lab1/task10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-man bash | tr -cs '[:alpha:]' '\n' | uniq -c | sort -nr | awk '{if (length($2 >= 4)) print $0}' | head -3
+man bash | tr -cs '[:alpha:]' '\n' | sort | uniq -ci | sort -nr | awk '{if (length($2) >= 4) print $0}' | head -3
